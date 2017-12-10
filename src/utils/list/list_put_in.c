e6facf1fd88f888385cb2e4e9afb17ea9c1c2fff
Vinchenzo Guibert <vinchenzo.guibert@epitech.eu>
2017-12-10 19:27:17 +0100

    Change list_put_in to add to the end of the list

--- src/utils/list/list_put_in.c
+++ src/utils/list/list_put_in.c
@@ -11,10 +11,16 @@
 int put_in_list(list_t **list, void *data)
 {
 	list_t *element;
+	list_t *tmp = *list;
 
 	element = malloc(sizeof(*element));
 	element->data = data;
-	element->next = *list;
-	*list = element;
+	element->next = NULL;
+	if (list && *list) {
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = element;
+	} else	
+		*list = element;
 	return (0);
 }
