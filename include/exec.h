d8a3734fc12a47087db5481adddbb4ef83ac5041
Kevin Freire <kevin.freire@epitech.eu>
2017-12-12 20:53:19 +0100

    exec.h edited to get the get_access.c

--- include/exec.h
+++ include/exec.h
@@ -12,5 +12,6 @@ int write_file(char *file_name);
 int append_file(char *file_name);
 int read_file(char *file_name);
 void read_stdin(char *word, int fd);
+char *get_access(char *cmd, list_t *l_env);
 
 #endif /* EXEC_H_ */
