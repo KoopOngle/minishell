6c8d5a254a7954a0e60d600a6ab880508ec36d63
Kevin Freire <kevin.freire@epitech.eu>
2017-12-14 18:05:16 +0100

    fixed the balance of the tree at the source

--- src/utils/btree/btree_insert_data.c
+++ src/utils/btree/btree_insert_data.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include "btree.h"
 #include "string.h"
+#include "my_printf.h"
 
 static int get_prio(char *str)
 {
@@ -21,6 +22,27 @@ static int get_prio(char *str)
 		return (0);
 }
 
+static void insert_data_nosep(btree_t **root, char **value, int is_first_op)
+{
+	if ((*root)->left == NULL)
+		btree_insert_data(&(*root)->left, value, is_first_op);
+	else
+		btree_insert_data(&(*root)->right, value, is_first_op);
+}
+
+static void insert_data_sep(btree_t **root, char **value, int is_first_op)
+{
+	btree_t *new_node;
+		
+	if (get_prio((*root)->right->value[0]) == 1)
+		btree_insert_data(&(*root)->right, value, is_first_op);
+	else {
+		new_node = btree_create_node(value);
+		new_node->left = (*root)->right;
+		(*root)->right = new_node;
+	}
+}
+
 void btree_insert_data(btree_t **root, char **value, int is_first_op)
 {
 	int comp;
@@ -29,19 +51,13 @@ void btree_insert_data(btree_t **root, char **value, int is_first_op)
 	if (root != NULL && *root != NULL) {
 		comp = get_prio(value[0]);
 		if (comp == 0)
-			btree_insert_data(&(*root)->right, value, is_first_op);
-		else if (comp == 2 || is_first_op == 1) {
+			insert_data_nosep(root, value, is_first_op);
+	        else if (comp == 2 || is_first_op == 1) {
 			new_node = btree_create_node(value);
 			new_node->left = *root;
 			*root = new_node;
-		} else {
-			new_node = btree_create_node(value);
-			new_node->left = (*root)->right;
-			(*root)->right = new_node;
-			//btree_insert_data(&(*root)->right, value, is_first_op);
-			//new_node = btree_create_node(value);
-			//(*root)->right = new_node;
-		}
+		} else
+			insert_data_sep(root, value, is_first_op);
 	} else if (root != NULL && *root == NULL)
 		*root = btree_create_node(value);
 }
