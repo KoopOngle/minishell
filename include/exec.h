59b3e81a4e87c219ed1ee92d8c373beda1c4fd61
Kevin Freire <kevin.freire@epitech.eu>
2017-12-15 10:44:30 +0100

    update exec.h adding 2 function : create_cmd & create_cmd_list

--- include/exec.h
+++ include/exec.h
@@ -11,6 +11,14 @@
 typedef struct btree btree_t;
 typedef struct list list_t;
 
+typedef struct cmd
+{
+        char **value;
+        int is_separator;
+} cmd_t;
+
+cmd_t *create_cmd(char **word_array);
+list_t *create_cmd_list(char **word_array);
 int write_file(char *file_name);
 int append_file(char *file_name);
 int read_file(char *file_name);
@@ -18,7 +26,6 @@ void read_stdin(char *word, int fd);
 char *get_access(char *cmd, list_t *l_env);
 void command_handler(btree_t *cmd, list_t *l_env);
 void my_exec(char **argv, list_t *l_env);
-
 int then(btree_t *btree, list_t *l_env, int my_stdin, int my_stdout);
 
 #endif /* EXEC_H_ */
