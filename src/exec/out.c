95c41fee8faeca54f2f529e6fb9e259626350f86
Vinchenzo Guibert <vinchenzo.guibert@epitech.eu>
2017-12-16 02:13:53 +0100

    Change out (>) function to return the file descriptor of the file

--- src/exec/out.c
+++ src/exec/out.c
@@ -9,25 +9,14 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include "btree.h"
 #include "my_printf.h"
 #include "list.h"
 #include "exec.h"
 #include "error.h"
 
-void out_handler(btree_t *btree, list_t *l_env, int my_stdin, int my_stdout)
+void out_handler(list_t *list, list_t *l_env, int *my_stdin, int *my_stdout)
 {
-	int fd = write_file(btree->right->value[0]);
-	int pid;
+	int fd = write_file(((cmd_t *)list->data)->value[0]);
 
-	if (fd < 0)
-		my_print_err("error opening file");
-	pid = fork();
-	if (pid == -1)
-		my_print_err("error fork");
-	else if (pid == 0) {
-		dup2(fd, 1);
-		my_exec(btree->left->value, l_env);
-	}
-	wait(NULL);
+	*my_stdout = fd;
 }
